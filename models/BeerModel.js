5e48478bb0f07ea279b399c31b45e2a67aa9879d
Leonid <golani04@gmail.com>
2016-04-11 17:24:39 +0300

    rating

--- models/BeerModel.js
+++ models/BeerModel.js
@@ -2,6 +2,7 @@ var BeerModel = Backbone.Model.extend({
    defaults: {
       name: '',
       style: '',
+      rating: 0,
       abv: null,
       imgUrl: ''
    }
--- views/BeerView.js
+++ views/BeerView.js
@@ -3,9 +3,32 @@ var BeerView = Backbone.View.extend({
 
    template: Handlebars.compile($('#beer-template').html()),
 
+   events: {
+      'click .rate-btn': 'rating'
+   },
+
+   rating: function (e) {
+      var avg = $('input[name="avg-rate"]:checked').val();
+      var rate = this.model.get('rating');
+      if(!isNaN(avg) && rate === 0) {
+         this.model.set('rating', Number(avg));
+      } else if (!isNaN(avg)) {
+         this.model.set('rating', (Number(avg)+rate)/2);
+      }
+
+      this.render();
+   },
+
+   removeBeer: function () {
+
+   },
+
+   editBeer: function () {
+
+   },
+
    render: function () {
       this.$el.html(this.template(this.model.toJSON()));
-      console.log(this.model.toJSON());
       return this;
    }
 });
